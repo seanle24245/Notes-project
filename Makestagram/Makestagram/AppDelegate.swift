3ebb625489d1ff91208807ec50a42535ea75869c
seanle24245 <sean.le24245@gmail.com>
2017-07-05 13:15:26 -0400

    step 6 makestagram

--- Makestagram/Makestagram/AppDelegate.swift
+++ Makestagram/Makestagram/AppDelegate.swift
@@ -13,13 +13,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)
-        -> Bool {
-            FirebaseApp.configure()
-            return true
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        // 1
+        let storyboard = UIStoryboard(name: "Login", bundle: .main)
+        
+        // 2
+        if let initialViewController = storyboard.instantiateInitialViewController() {
+            // 3
+            window?.rootViewController = initialViewController
+            // 4
+            window?.makeKeyAndVisible()
+        }
+        
+        return true
     }
-
-    
 
 
 
---
+++ Makestagram/Makestagram/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Sean Le on 7/5/17.
+//  Copyright © 2017 Sean Le. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButtonTapped(sender: Any) {
+        print("buttonTapped")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
